9acb304a46efaea8b1a8b486a0eddefd8a3a6262
oogiri-org <github@oogiri.org>
2016-05-06 01:57:14 +0900

    connection opened in init

--- Sources/Redis.swift
+++ Sources/Redis.swift
@@ -2,9 +2,10 @@ import TCP
 
 public final class Redis: Commands {
 
-	public var conn: TCPConnection
+    public var conn: TCPConnection
 
-	public init(_ host: String, _ port: Int) throws {
+    public init(_ host: String, _ port: Int) throws {
         conn = try TCPConnection(host: host, port: port)
-	}
+        try conn.open()
+    }
 }
